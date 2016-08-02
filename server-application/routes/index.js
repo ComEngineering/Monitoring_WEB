1d7ddacc0a1618794dac650a416f65476eb230c2
alexandr <alexandr.pryadka@gmail.com>
2016-08-02 22:03:41 +0200

    весь html контент реорганизован в одну страницу

--- server-application/routes/index.js
+++ server-application/routes/index.js
@@ -24,42 +24,6 @@ router.get('/', isAuthenticated, function(req, res, next) {
     res.render('home', { title: 'Teko' });
 });
 
-router.get('/system-power', isAuthenticated, function(req, res, next) {
-    res.render('system-power', { title: 'Teko' });
-});
-
-router.get('/system-logs', isAuthenticated, function(req, res, next) {
-    res.render('system-logs', { title: 'Teko' });
-});
-
-router.get('/options-time', isAuthenticated, function(req, res, next) {
-    res.render('options-time', { title: 'Teko' });
-});
-
-router.get('/options-net', isAuthenticated, function(req, res, next) {
-    res.render('options-net', { title: 'Teko' });
-});
-
-router.get('/options-modbus', isAuthenticated, function(req, res, next) {
-    res.render('options-modbus', { title: 'Teko' });
-});
-
-router.get('/modules-main', isAuthenticated, function(req, res, next) {
-    res.render('modules-main', { title: 'Teko' });
-});
-
-router.get('/climats-cond', isAuthenticated, function(req, res, next) {
-    res.render('climats-cond', { title: 'Teko' });
-});
-
-router.get('/climats-nagrevateli', isAuthenticated, function(req, res, next) {
-    res.render('climats-nagrevateli', { title: 'Teko' });
-});
-
-router.get('/climats-pelte', isAuthenticated, function(req, res, next) {
-    res.render('climats-pelte', { title: 'Teko' });
-});
-
 function isAuthenticated(req, res, next) {
     if (req.isAuthenticated())
         return next();
